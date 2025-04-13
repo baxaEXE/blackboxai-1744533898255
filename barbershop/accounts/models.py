66734d5153175336bdcc350d6ecc09450ade0be2
Bakhadir <135311610+baxaEXE@users.noreply.github.com>
2025-04-13 13:45:58 +0500

    Add barbershop/accounts/models.py 1744533958098

---
+++ barbershop/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+class User(AbstractUser):
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'."
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.username
